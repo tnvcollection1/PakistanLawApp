0d20698f7ed1c54bccdd486d21ba6f4c3619df04
tnvcollection1 <tnvcollection1@gmail.com>
2026-08-06 12:27:59 +0530

    feat: real frontend core files from VPS

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
-import CaseSearchBox from '../components/CaseSearchBox';
-import SidebarLayout from '../components/SidebarLayout';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SidebarLayout from './components/SidebarLayout';
+import HomePage from './pages/HomePage';
+import CitationSearchPage from './pages/CitationSearchPage';
+import CasesPage from './pages/CasesPage';
+import CourtsPage from './pages/CourtsPage';
+import LawsPage from './pages/LawsPage';
+import JournalsPage from './pages/JournalsPage';
 
 function App() {
   return (
-    <SidebarLayout>
-      <div className="space-y-6">
-        <CaseSearchBox />
-      </div>
-    </SidebarLayout>
+    <Router>
+      <SidebarLayout>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/citation-search" element={<CitationSearchPage />} />
+          <Route path="/cases" element={<CasesPage />} />
+          <Route path="/courts" element={<CourtsPage />} />
+          <Route path="/laws" element={<LawsPage />} />
+          <Route path="/journals" element={<JournalsPage />} />
+        </Routes>
+      </SidebarLayout>
+    </Router>
   );
 }
 
