3c1699f2ed1d9e7e3b93255466f8d144f40b3908
tnvcollection1 <tnvcollection1@gmail.com>
2026-09-09 07:12:05 +0530

    feat: add LawBot route to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,20 +7,26 @@ import CasesPage from './pages/CasesPage';
 import CourtsPage from './pages/CourtsPage';
 import LawsPage from './pages/LawsPage';
 import JournalsPage from './pages/JournalsPage';
+import LawBotPage from './pages/LawBotPage';
 
 function App() {
   return (
     <Router>
-      <SidebarLayout>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/citation-search" element={<CitationSearchPage />} />
-          <Route path="/cases" element={<CasesPage />} />
-          <Route path="/courts" element={<CourtsPage />} />
-          <Route path="/laws" element={<LawsPage />} />
-          <Route path="/journals" element={<JournalsPage />} />
-        </Routes>
-      </SidebarLayout>
+      <Routes>
+        <Route path="/lawbot" element={<LawBotPage />} />
+        <Route path="*" element={
+          <SidebarLayout>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/citation-search" element={<CitationSearchPage />} />
+              <Route path="/cases" element={<CasesPage />} />
+              <Route path="/courts" element={<CourtsPage />} />
+              <Route path="/laws" element={<LawsPage />} />
+              <Route path="/journals" element={<JournalsPage />} />
+            </Routes>
+          </SidebarLayout>
+        } />
+      </Routes>
     </Router>
   );
 }
